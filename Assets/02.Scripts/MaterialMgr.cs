be71b398fd22cc5719a412d375af0552e16e5502
Kwonkunkun <59603575+Kwonkunkun@users.noreply.github.com>
2020-11-19 12:54:28 +0900

    upload project

---
+++ Assets/02.Scripts/MaterialMgr.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialMgr : MonoBehaviour
+{
+    public Material ball;
+    public Material seaFood;
+
+    public float attenstionVal = 0.0f;
+    public float meditationVal = 0.0f;
+
+    void Update()
+    {
+        CalculateValue();
+        ChangeIntensity();
+    }
+
+    void CalculateValue()
+    {
+        attenstionVal = (float)((float)DisplayData.Instance.Attention / 100.0f);
+        meditationVal = (float)((float)DisplayData.Instance.Meditation / 100.0f);
+    }
+
+    void ChangeIntensity()
+    {
+        ball.SetFloat("_intensity", attenstionVal);
+        seaFood.SetFloat("_intensity", meditationVal * 1.5f);
+    }
+}
---
+++ Assets/02.Scripts/TestCodeMgr.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCodeMgr : MonoBehaviour
+{
+    //0 ~ 100
+    //0 ~ 1
+    public Material mat;
+
+    void Update()
+    {
+        ChangeIntensity();
+    }
+
+    void ChangeIntensity()
+    {
+        mat.SetFloat("_intensity", (float)((float)DisplayData.Instance.Attention/100.0f));
+    }
+}
